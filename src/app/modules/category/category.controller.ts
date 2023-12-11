a039efe7029f352dddfd560cdcf6c2fd7e3605bd
Adnan-Sarkar <adnansarkar111@gmail.com>
2023-12-11 18:00:48 +0600

    Created category controller for create category

---
+++ src/app/modules/category/category.controller.ts
@@ -0,0 +1,16 @@
+import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { CategoryServices } from "./category.service";
+
+// create a category
+const createCategory = catchAsync(async (req, res) => {
+  const result = await CategoryServices.createCategoryIntoDB(req.body);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: "Category created successfully",
+    data: result,
+  });
+});
