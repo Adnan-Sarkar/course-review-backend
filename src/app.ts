aeaafa0a16a3fe2139810338dcdd88344fc5062e
Adnan-Sarkar <adnansarkar111@gmail.com>
2023-12-11 16:52:54 +0600

    Add global route in app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
+import globalRouter from "./app/router/router";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+// route
+app.use("/api", globalRouter);
 
 export default app;
