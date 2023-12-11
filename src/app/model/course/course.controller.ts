6955e72c59134cf04af3cbc81dd34371d9cb9d22
Adnan-Sarkar <adnansarkar111@gmail.com>
2023-12-11 15:51:13 +0600

    Created course controller and route

---
+++ src/app/model/course/course.controller.ts
@@ -0,0 +1,22 @@
+import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { CourseServices } from "./course.service";
+
+// create a course
+const createCourse = catchAsync(async (req, res) => {
+  const result = await CourseServices.createCourseIntoDB(req.body);
+
+  console.log({ controller: result });
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: "Course created successfully",
+    data: result,
+  });
+});
+
+export const CourseController = {
+  createCourse,
+};
