a15a3cbc1a56a58fcae0e0d25ef1766b5f26fc21
Adnan-Sarkar <adnansarkar111@gmail.com>
2023-12-13 00:34:52 +0600

    Implement not found route and global error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import globalRouter from "./app/router/router";
+import notFound from "./app/middleware/notFound";
+import globalErrorHandler from "./app/middleware/globalErrorHandler";
 
 const app = express();
 
@@ -9,5 +11,11 @@ app.use(cors());
 
 // route
 app.use("/api", globalRouter);
+
+// API route not found
+app.all("*", notFound);
+
+// Global error handler
+app.use(globalErrorHandler);
 
 export default app;
