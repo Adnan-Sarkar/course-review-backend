0ca4f1c3138d570e3a5e317cf8b3fd76bf6d1526
Adnan-Sarkar <adnansarkar111@gmail.com>
2023-12-13 20:03:22 +0600

    Created home route

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import globalRouter from "./app/router/router";
 import notFound from "./app/middleware/notFound";
@@ -11,6 +11,16 @@ app.use(cors());
 
 // route
 app.use("/api", globalRouter);
+
+app.get("/", (_req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to the course review backend API",
+    details: "For API details, visit /api/users",
+    documentation:
+      "For detailed documentation, visit: https://github.com/Porgramming-Hero-web-course/l2b2a3-course-review-Adnan-Sarkar",
+  });
+});
 
 // API route not found
 app.all("*", notFound);
